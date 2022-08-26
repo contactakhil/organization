cf01b2ed3e3065be1fc93c22415193bc66eec7d4
contactakhil <112102458+contactakhil@users.noreply.github.com>
2022-08-26 17:01:51 +0530

    updted files

--- employees/urls.py
+++ employees/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('update-employee/<str:pk>', views.update_employee),
     path('delete-employee/<str:pk>', views.delete_employee),
     path('add-employee/', views.add_employee),
+    path('list-in-range/<num1>/<num2>', views.list_range),
+    path('list-range-field/<field1>/<num1>/<num2>/', views.list_range_field),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Q
 
 from .serializers import EmployeesSerializer
 
@@ -41,3 +42,16 @@ def delete_employee(request, pk):
     employee = Employees.objects.get(id=pk)
     employee.delete()
     return Response('Employee deleted')
+
+@api_view(['GET'])
+def list_range(request, num1, num2):
+    query = Employees.objects.filter(total_salry__range = [int(num1), int(num2)])
+    serializer = EmployeesSerializer(query, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def list_range_field(request, field1, num1, num2):
+    field_filter = field1 + '__range'
+    query = Employees.objects.filter(field_filter = [int(num1), int(num2)])
+    serializer = EmployeesSerializer(query, many=True)
+    return Response(serializer.data)
